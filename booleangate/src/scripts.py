ef20fcc370f3cca16545462a9e321e9de1198da9
SirrorsMoore1975 <chadwickau@hotmail.com>
2024-02-08 22:52:34 +0800

    setup basic structure for boolengate exercise

---
+++ booleangate/src/scripts.py
@@ -0,0 +1,14 @@
+"""
+booleangate_not\n
+@param {expression} ??? the input conditional operand expression that expecting boolean \n
+@returns {any} ??? return inverse of the input boolean\n
+
+boolengate
+@param {"and|or|nand|nor"} ??? the logic gate expected the output to behave according to input 
+@param {expression} ??? the first operand expression
+@param {expression} ??? the second operand expression
+@returns {any} ??? the expression expected depends on the logic gate 
+"""
+
+def booleangate_not(A):
+    return not A
