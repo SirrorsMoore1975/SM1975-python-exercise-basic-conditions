6c981abf70075564385e7c1fdf1f27910a268875
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-03-05 17:41:17 +0800

    add basic scripts_test.py

--- Exercises/06_calculation/test/test_calculation/scripts_test.py
+++ Exercises/06_calculation/test/test_calculation/scripts_test.py
@@ -1,7 +1,7 @@
 import pytest
 from src.scripts import calculation
 
-@pytest.mark.parametrize("sign, list, value, expected",[
+@pytest.mark.parametrize("sign, lst, value, expected",[
     ("add",[11,12,13,14,15],5, [16,17,18,19,20]),
     ("add",[-1, 2, -3, 4, -5], 5, [4, 7, 2, 9, 0]),
     ("add",[-1, -2, -3, -4, -5], 5, [4, 3, 2, 1, 0]),
@@ -9,3 +9,7 @@ from src.scripts import calculation
     ("subtract", [11, 12, 13, 14, 15], 10, [1,2,3,4,5]),
     ("subtract", [6,7,8,9,10], 10, [-4, -3, -2, -1, 0]),
 ])
+
+def test_calculation(sign, lst, value, expected ):
+    result = calculation(sign, lst, value)
+    assert result == expected
