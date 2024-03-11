f08a43a612aead23218f2664689940eb1c3f4cb0
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-03-11 19:11:07 +0800

    feat: complete basic test and can pass test with solutions

--- Exercises/06_calculation/src/scripts.py
+++ Exercises/06_calculation/src/scripts.py
@@ -8,5 +8,30 @@
 
 
 
-def calculation(sign, x, value):
-    pass
+def calculation(sign: str, x: list, value: int) -> list:
+    allowed = ["add", "subtract", "multiply", "divide", "getRemainder"]
+    if sign not in allowed:
+        return "sign type used is undefined"
+    elif type(x) != list:
+        return "x is not a list"
+    elif not isinstance(value, (int, float, complex)):
+        raise ValueError("{0} is not numeric".format(value))
+   
+    
+    match sign:
+        case "add":
+            result = [y + value for y in x]
+        case "subtract":
+            result = [y - value for y in x]
+        case "multiply":
+            result = [y * value for y in x]
+        case "divide":
+            result = []
+            for y in range(len(x)):
+                if x[y] == 0 or value == 0:
+                    result.append(0)
+                else:
+                    result.append(x[y] / value)
+        case "getRemainder":
+            result = [y % value for y in x]
+    return result
