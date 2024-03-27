def14a73d9d2aa37ebf59a01cf3d8fd2a9064190
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-03-27 11:53:15 +0800

    amend space for a test

--- Exercises/07_addStr/test/test_addStr/scripts_test.py
+++ Exercises/07_addStr/test/test_addStr/scripts_test.py
@@ -4,7 +4,7 @@ from src.scripts import addStr
 @pytest.mark.parametrize("input,expected",[
     ((1,2,3,4),"1234"),
     (("a","b","c","d","e"), "abcde"),
-    (("five"," ", "dollar"), "fivedollar")
+    (("five"," ", "dollar"), "five dollar")
 ])
 
 def test_addStr(input, expected):
