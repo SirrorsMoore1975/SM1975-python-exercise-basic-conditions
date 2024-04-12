2c06ca2c09dde2b9f365e1ea61bea63267ea3e08
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-04-12 18:45:35 +0800

    wip addStr

--- Exercises/07_addStr/README.md
+++ Exercises/07_addStr/README.md
@@ -1,3 +1,3 @@
 # addStr
-Wrtie a function called addStr that can add a number of strings in the parameter into one string
+Wrtie a function called addStr that can add a number parameter and a string parameter and return one string
 
--- Exercises/07_addStr/src/scripts.py
+++ Exercises/07_addStr/src/scripts.py
@@ -1,2 +1,19 @@
-def addStr(*arg):
-    pass
+def addStr(args:tuple):
+    
+    try:
+        result = ""
+        for i in args:
+            if type(i) == 'int':
+                result += str(i)
+            else:
+                result += i
+        return result
+    except TypeError:
+        return "TypeError: args not tuple"
+    
+    
+    # for l in args:
+    #     if type(l) == 'int':
+    #         l = str(l)
+    #     result += l
+    
