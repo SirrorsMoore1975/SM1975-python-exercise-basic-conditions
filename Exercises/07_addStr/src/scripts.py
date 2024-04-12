0aef938e8b3372d61da3875a2d2e0f2e27c4d415
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-04-12 18:52:10 +0800

    test 1234 passing

--- Exercises/07_addStr/src/scripts.py
+++ Exercises/07_addStr/src/scripts.py
@@ -1,19 +1,6 @@
 def addStr(args:tuple):
-    
-    try:
-        result = ""
-        for i in args:
-            if type(i) == 'int':
-                result += str(i)
-            else:
-                result += i
-        return result
-    except TypeError:
-        return "TypeError: args not tuple"
-    
-    
-    # for l in args:
-    #     if type(l) == 'int':
-    #         l = str(l)
-    #     result += l
+    result = ""
+    for i in args:
+        result += str(i)
+    return result
     
