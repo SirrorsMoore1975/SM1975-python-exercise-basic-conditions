d40ac15c36fc2b94d1866e57a26040e9f9e8209b
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-06-18 17:51:31 +0800

    feat: test to check append

--- Exercises/11_push/test/test_push/scripts_test.py
+++ Exercises/11_push/test/test_push/scripts_test.py
@@ -1,6 +1,26 @@
 import pytest
 from src.scripts import push
+import inspect
 
+def has_append(func):
+    source_lines, _ = inspect.getsourcelines(func)
+    source_code = ''.join(source_lines)
+    return 'append(' in source_code
+
+def test_push_has_no_append():
+    assert has_append(push) is False
+
+@pytest.mark.parametrize("_list, element, expected_list, expected_length",[
+    ([1,2,3,4], 5, [1,2,3,4,5], 5),
+    ([2,4,6,8], 10, [2,4,6,8,10], 5),
+    ([1,3,5,7], 9, [1,3,5,7,9], 5),
+    ([1,7,2,4,9], 6, [1,7,2,4,9,6], 6)
+])
+
+def test_push(_list, element, expected_list, expected_length):
+    result = push(_list, element)
+    assert _list == expected_list
+    assert result == expected_length
 
 # def detect_append(script_code):
 #     with open(script_code, 'r') as script:
@@ -22,12 +42,12 @@ from src.scripts import push
 #     return visitor.has_append
     
 
-@pytest.mark.detect_append
-def test_append_usage():
-    input = [1,2,3,4]
-    result = push(input, 5)
-    assert result == 5
-    assert input == [1,2,3,4,5]
+# @pytest.mark.detect_append
+# def test_append_usage():
+#     input = [1,2,3,4]
+#     result = push(input, 5)
+#     assert result == 5
+#     assert input == [1,2,3,4,5]
 
     
 
