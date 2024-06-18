982ff110501a56f73c2fe4743cf42654f0def411
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2024-06-18 18:07:11 +0800

    remove comment out scripts

--- Exercises/11_push/test/test_push/scripts_test.py
+++ Exercises/11_push/test/test_push/scripts_test.py
@@ -14,55 +14,11 @@ def test_push_has_no_append():
     ([1,2,3,4], 5, [1,2,3,4,5], 5),
     ([2,4,6,8], 10, [2,4,6,8,10], 5),
     ([1,3,5,7], 9, [1,3,5,7,9], 5),
-    ([1,7,2,4,9], 6, [1,7,2,4,9,6], 6)
+    ([1,7,2,4,9], 6, [1,7,2,4,9,6], 6),
+    (["a","b","c","d"], "e", ["a","b","c","d","e"], 5),
 ])
 
 def test_push(_list, element, expected_list, expected_length):
     result = push(_list, element)
     assert _list == expected_list
     assert result == expected_length
-
-# def detect_append(script_code):
-#     with open(script_code, 'r') as script:
-#         codes = script.read()
-#     tree = ast.parse(codes)
-
-#     class AppendVisitor(ast.NodeVisitor):
-#         def __init__(self):
-#             self.has_append = False
-        
-#         def visit_Call(self, node):
-#             if isinstance(node.func, ast.Attribute) and node.func.attr == 'append':
-#                 self.has_append = True
-#             self.generic_visit(node)
-    
-#     visitor = AppendVisitor()
-#     visitor.visit(tree)
-
-#     return visitor.has_append
-    
-
-# @pytest.mark.detect_append
-# def test_append_usage():
-#     input = [1,2,3,4]
-#     result = push(input, 5)
-#     assert result == 5
-#     assert input == [1,2,3,4,5]
-
-def test01_push():
-    input = [1,2,3,4]
-    result = push(input, 5)
-    assert result == 5
-    assert input == [1,2,3,4,5]
-
-def test02_push():
-    input = ["a", "b", "c"]
-    result = push(input, "d")
-    assert result == 4
-    assert input == ["a", "b", "c", "d"]
-
-
-# pytest.mark.parametrize('input,expected',[()])
-# def test_push(input, expected):
-#     result=push(input)
-#     assert result == expected
