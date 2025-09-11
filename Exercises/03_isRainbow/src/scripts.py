1f010f4508e68ae5ac417015ffd4a62018293c77
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2025-09-11 16:06:12 +0800

    wip: correcting isRainbow scripts file

--- Exercises/03_isRainbow/src/scripts.py
+++ Exercises/03_isRainbow/src/scripts.py
@@ -1,19 +1,24 @@
-"""
-/**
- * @param {string} ??? - the color to check
- * @returns {boolean} whether or not the given color is a rainbow color
- */
-"""
+
 import json
-with open("src/rainbow.json") as rainbow:
+with open("rainbow.json", "r") as rainbow:
     payload = json.load(rainbow)
     rainbowColor = payload[0]["color"]
 
 
-def isRainbowColor(color):
+def isRainbowColor(color: str) -> bool:
+    """_summary_
+
+    Args:
+        color (_str_): _string color name to be tested_
+
+    Returns:
+        _bool_: _expected boolean output_
+    """
     #print(type(rainbowColor))
-    for targetColor in rainbowColor:
-        if targetColor == color.lower():
-            #print(targetColor)
-            return True
-    return False
+    # for targetColor in rainbowColor:
+    #     if targetColor == color.lower():
+    #         #print(targetColor)
+    #         return True
+    # return False
+    print(rainbowColor.read())
+    return any(targetColor == color.lower() for targetColor in rainbowColor)
