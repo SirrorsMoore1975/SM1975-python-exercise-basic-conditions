9c3feb5ef002051f519ede008f7bf64736bee88c
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2025-09-19 16:32:11 +0800

    wip: preparing users data, legal age to test, writing fn aiding test_scripts

--- Exercises/12_say_hellos/test/test_say_hellos/scripts_test.py
+++ Exercises/12_say_hellos/test/test_say_hellos/scripts_test.py
@@ -1,6 +1,62 @@
 import pytest
 from src.scripts import say_hellos
+
+USERNAME="username"
+AGE="age"
+LANGUAGE="language"
+ESTONIAN="estonian"
+FILIPINO="filipino"
+HAWAIIAN="hawaiian"
+JAPANESE="japanese"
+ROMANIAN="romanian"
+TAIWANESE="taiwanese_chinese"
+
+users_payload=[
+    {
+        USERNAME:"Karen",
+        AGE:18,
+        LANGUAGE:JAPANESE
+    },
+    {
+        USERNAME:"Robert",
+        AGE:23,
+        LANGUAGE:ROMANIAN
+    },
+    {
+        USERNAME:"Quentin",
+        AGE:20,
+        LANGUAGE:FILIPINO
+    },
+    {
+        USERNAME:"Issac",
+        AGE:22,
+        LANGUAGE:TAIWANESE
+    },
+    {
+        USERNAME:"Charlie",
+        AGE:31,
+        LANGUAGE:HAWAIIAN
+    },
+    {
+        USERNAME:"Lopez",
+        AGE:19,
+        LANGUAGE:ESTONIAN
+    }
+]
+
+legal_adult_age=[18,22,25]
+
+def creating_message_to_greet(name, lang):
+    pass
+
+
+def amend_legal_age(age):
+    pass
+
+
 pytest.mark.parametrize('input,expected',[()])
+
 def test_say_hellos(input, expected):
+    
     result=say_hellos(input)
     assert result == expected
