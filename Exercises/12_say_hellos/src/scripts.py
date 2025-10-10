b459392174685ce37e6b9ef9dac6aa467af6bbda
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2025-10-10 19:06:15 +0800

    wip: missing lang parameter error

--- Exercises/12_say_hellos/src/scripts.py
+++ Exercises/12_say_hellos/src/scripts.py
@@ -9,17 +9,20 @@ class Greetings:
         with open("languages.json", "r", encoding="utf-8") as lang_json:
             self.lang_json = json.load(lang_json)
         self.lang_list = [lang.keys() for lang in self.lang_json]
+        ENGLISH="english_british"
+        self.default_lang = ENGLISH
     
     def say_hellos(self, person, lang):
         # YOUR CODE HERE
         JAPANESE="japanese"
-        ENGLISH="english_british"
+        if not lang:
+            lang = self.default_lang
         lang = lang.lower()
         if lang == JAPANESE:
             person = person + "-san"
         if self.lang_list in lang:
             return f"{self.lang_json[lang]} {person}."
-        return f"{self.lang_json[ENGLISH]} {person}."
+        return f"{self.lang_json[self.default_lang]} {person}."
     
     def amend_hello(self):
         # YOUR CODE HERE
