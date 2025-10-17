f6686e0602c39cd5de46cdca37f5060c53a2d81f
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2025-10-17 16:08:43 +0800

    feat:say_hellos test passed

--- Exercises/12_say_hellos/src/scripts.py
+++ Exercises/12_say_hellos/src/scripts.py
@@ -4,7 +4,7 @@ class Greetings:
     """
     class Greetings
     """
-    def __init__(self) -> str :
+    def __init__(self):
         # YOUR CODE HERE
         ENGLISH="english_british"
         self.default_lang = ENGLISH
@@ -12,13 +12,13 @@ class Greetings:
             self.data = json.load(lang_json)
         if not isinstance(self.data, list):
             print(ValueError("the json file is not a list"))
-        
-        unique_keys = set()
-        for item in self.data:
-            unique_keys.update(item.keys())
-            if item.keys() == ENGLISH:
-                self.default_hello = item.values()
-        self.unique_lang_list = list(str(unique_keys))
+        self.default_hello = self.return_hellos(ENGLISH)
+        # unique_keys = set()
+        # for item in self.data:
+        #     unique_keys.update(item.keys())
+        #     if item.keys() == ENGLISH:
+        #         self.default_hello = item.values()
+        # self.unique_lang_list = list(str(unique_keys))
         # for each_lang in self.df_lang_list:
         #     if self.df_lang_list[each_lang] == 
         #     self.lang_list = [] + [str(x.keys()) for x in each_lang]
@@ -26,25 +26,24 @@ class Greetings:
     def return_hellos(self,lang:str) -> str:
         lang = lang.lower()
         for y in self.data:
-            for x in y:
-                if lang == x.keys():
-                    return x.values()
+            for key,value in y.items():
+                if lang == key:
+                    return value
         return self.default_hello
         
     def say_hellos(self, person:str, lang:str)->str:
         # YOUR CODE HERE
         JAPANESE="japanese"
-        print(self.return_hellos(lang))
+        AMERICAN="english_american"
+        
         if not lang:
             lang = self.default_lang
         lang = lang.lower()
         if lang == JAPANESE:
             person = person + "-san"
-        if self.unique_lang_list in lang:
-            for item in self.data:
-                if item.keys() == lang:
-                    return f"{item[lang]} {person}."
-        return f"{self.default_hello} {person}."
+        if lang not in (AMERICAN, self.default_lang):
+            return f"{self.return_hellos(lang)} {person}."
+        return f"{self.return_hellos(self.default_lang)} {person}."
     
     def amend_hello(self):
         # YOUR CODE HERE
