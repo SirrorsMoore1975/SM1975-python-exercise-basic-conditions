2b8f4b9852fc2277b8c68d01ab4cc1a6ed104956
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2025-12-01 14:57:14 +0800

    remove useless return None
    remove space, breaking line piecemeal
    
    add & remove docstring contents

--- Exercises/12_say_hellos/src/scripts.py
+++ Exercises/12_say_hellos/src/scripts.py
@@ -1,8 +1,11 @@
+"""
+    _SOLUTION BRANCH_
+"""
 import json
 
 class Greetings:
     """
-    class Greetings
+        class Greetings
     """
     def __init__(self):
         # YOUR CODE HERE
@@ -12,17 +15,10 @@ class Greetings:
             print(ValueError("the json file is not a list"))
         self.additional_lang=[]
         self.default_age = 20
-    
+
     def return_hellos(self,lang:str) -> str:
         """
-            _An internal helper function to ease say_hellos.
-            It return the hello lay down in lang_list. If the script found the hello message of that language, it return that hello in that language. If the script cannot found the language, it return 'How are you?'._
-            
-            Args: 
-                lang (str): _expected the language name._
-            
-            Returns:
-                str: _return a greeting of the supported language, return `How are you?` if language not supported._
+            Helper function helping say_hellos
         """
         lang = lang.lower()
         def check_lang_list(lang_list):
@@ -38,28 +34,31 @@ class Greetings:
             if result is not None:
                 return result
         return "How are you?"
-        
+
     def say_hellos(self, person:str="Anonymous", lang:str="british_english")->str:
         # YOUR CODE HERE
         """
             _The class function that return the hello of a given language_
 
         Args:
-            person (str, optional): _The name of the person who the script shall address to_. Defaults to "Anonymous".
-            lang (str, optional): _The language to greet, if supported, to the person_. Defaults to "british_english".
+            person (str): _The name of the person who the script shall address to. Default: "Anonymous"._
+            lang (str): _The language to greet, if supported, to the person. Default: "british_english"._
 
         Returns:
-            str: _return the greeting statement address to the given person on that given language, if available,_
+            (dict): _return the greeting statement address to the given person on that given language, if available,_
         """
         JAPANESE="japanese"
-        
+
         lang = lang.lower()
         if lang == JAPANESE:
             person = person + "-san"
         return f"{self.return_hellos(lang)} {person}."
-    
+
     def amend_hello(self, amend_lang:str, amend_hello:str):
         # YOUR CODE HERE
+        """
+            _amend or amend added hello of a language_
+        """
         amend_lang = amend_lang.lower()
         for data in self.additional_lang:
             for key, value in data.items():
@@ -75,10 +74,14 @@ class Greetings:
                     return None
         print(f'amend_hello: No matching {amend_hello} found. use add_hellos')
         return None
-    
+
     def add_hello(self, add_lang:str, hello_msg:str) -> None:
         # YOUR CODE HERE
-        """_Add hello to a specified language with the hello message. The script should accept one entry. Further same language will not write to the given class. If required amendment to the language, use amend_hello instead._
+        """
+            _Add hello to a specified language with the hello message. 
+            The script should accept one entry. 
+            Further same language will not write to the given class. 
+            If required amendment to the language, use amend_hello instead._
 
         Args:
             add_lang (str): _add language into the class_
@@ -103,18 +106,11 @@ class Greetings:
                 return None
         result = {add_lang:hello_msg}
         self.additional_lang.append(result)
-        return None
-    
+
     def reset_hello(self, remove_lang:str)-> None:
         # YOUR CODE HERE
         """
-            _delete specific hello to of the selected language_
-
-        Args:
-            remove_lang (str): _the language that you wanted to remove_
-
-        Returns:
-            _None_type_: _None is expected_
+            _reset the added or amended specific language_
         """
         remove_lang = remove_lang.lower()
         for idx,y in enumerate(self.additional_lang):
@@ -127,31 +123,45 @@ class Greetings:
     
     def reset_all_hellos(self) -> None:
         # YOUR CODE HERE
+        """
+            _Reset all hellos_
+        """
         self.additional_lang = []
         print("reset_all_hellos: All added hellos are removed and back to default.")
-        return None
-    
+
     def is_adult(self, age: int = 20) -> bool:
         # YOUR CODE HERE
+        """
+            _returns True if the age is adult, or else False_
+        """
         print(f"is_adult: age checking for {age}")
         return age >= self.default_age
-    
+
     def amend_adult_age(self, age :int = 20) -> None:
         # YOUR CODE HERE
+        """
+            _amend legal adult age_
+        """
         self.default_age = age
         print(f"amend_adult_age: legal adult age changed to {age}")
-        return None
-    
+
     def reset_adult_age(self) -> None:
         # YOUR CODE HERE
+        """
+            _reset adult age to default 20_
+        """
         self.default_age = 20
-        print(f"reset_adult_age: legal adult age changed back to 20")
-        return None
-    
+        print("reset_adult_age:")
+        print("legal adult age changed back to 20")
+
     def reset_to_default(self) -> None:
         # YOUR CODE HERE
+        """
+            _reset legal adult age to 20 and remove all add and amended additional language_
+        """
         self.default_age = 20
         self.additional_lang = []
-        print(f"reset_to_default: reset adult age: 20, reset add and amended language back to default")
-        return None
+        print("reset_to_default:")
+        print("reset adult age: 20")
+        print("reset add and amended language back to default")
     
