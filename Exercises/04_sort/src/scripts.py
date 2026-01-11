eb0ea665222eedec74617faa7c44e75280bb9695
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2026-01-12 01:48:52 +0800

    wip: adding different sort method as def

--- Exercises/04_sort/src/scripts.py
+++ Exercises/04_sort/src/scripts.py
@@ -16,8 +16,34 @@ Declare a function sort. Don’t use the built-in sort method.
 """
 
 
-def sort():
-    pass
+def sort(the_list):
+    sort_method = ["quick", "bubble", "selection", "insertion", "merge"]
+    if len(the_list) <= 1:
+        return the_list
+
+    def quick_sort(my_list):
+        pivot = my_list[0]
+        left_hand_side = [i for i in my_list[1:] if i <= pivot]
+        right_hand_side = [j for j in my_list[1:] if j > pivot]
+        return sort(left_hand_side) + [pivot] + sort(right_hand_side)
+
+    def bubble_sort(my_list):
+        n = len(my_list)
+        for i in range(n):
+            for j in range(0, n - 1 - i):
+                if my_list[j] > my_list[j + 1]:
+                    my_list[j], my_list[i] = my_list[j+1], my_list[j]
+        return my_list
+    
+    def selection_sort(my_list):
+        return None
+
+    def inserction_sort(my_list):
+        return None
+    
+    def merge_sort(my_list):
+        return None
+    return None
 
 
 
