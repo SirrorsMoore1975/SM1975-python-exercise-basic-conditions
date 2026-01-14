e3ddf12eacf0aca5444609480110c34f4074995a
SirrorsMoore1975 <chadwickau@hotmail.com>
2026-01-14 19:19:16 +0800

    fix add string function, add docstring

--- Exercises/07_addStr/src/scripts.py
+++ Exercises/07_addStr/src/scripts.py
@@ -1,3 +1,17 @@
-def addStr():
-    pass
-    
+"""
+    adding tuple of elements together
+"""
+
+def addStr(tuple_args:tuple):
+    """
+        add tuple of elements together into one string
+
+    Args:
+        tuple_args(tuple): tuple contain elements
+    Returns:
+        returns (str): return a combined string
+    """
+    result = ""
+    for element in tuple_args:
+        result += str(element)
+    return result
