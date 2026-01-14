a2371934a2fdb5b858b2e3f3facb43111b5bf000
SirrorsMoore1975 <chadwickau@hotmail.com>
2026-01-14 16:34:05 +0800

    fix solution for 05 return data type

--- Exercises/05_returnDatatype/src/scripts.py
+++ Exercises/05_returnDatatype/src/scripts.py
@@ -3,5 +3,15 @@
 @returns {string} {"number" | "boolean"| "dictionary" | "float" | "list" | "dictionary" | "tuple" | "range" | "None Type" |  "complex"  } return the name of the input parameter's datatype 
 """
 
-def returnDatatype():
-    pass
+def returnDatatype(input) -> str:
+    datatype = {
+        str : "string",
+        int : "number",
+        bool : "boolean",
+        dict : "dictionary",
+        type(None): "None Type"
+    }
+    data_type = type(input)
+    if data_type in datatype:
+        return datatype[data_type]
+    return data_type.__name__
