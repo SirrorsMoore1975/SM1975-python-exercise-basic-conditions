16f793e47e57d46c892e6162b8afceda7cc9bc1e
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2026-01-18 16:20:52 +0800

    fix: is odd without if

--- Exercises/09_isOddWithoutIf/src/scripts.py
+++ Exercises/09_isOddWithoutIf/src/scripts.py
@@ -1,3 +1,10 @@
-def isOddWithoutIf():
-    pass
+def isOddWithoutIf(num:int) -> bool:
+    """
+        Return true or false for number that is odd without using if statement
 
+        Args:
+            num (int): number to be check if it is odd
+        Returns:
+            return (bool): True or False if number is odd or not odd
+    """
+    return not num % 2 == 0
